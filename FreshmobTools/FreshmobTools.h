828124aba258b542b00924a0270ff31c079d84fa
Rogerio de Paula Assis <rogerio@freshmob.com.au>
2016-11-17 09:21:17 +1100

    Initial Commit

---
+++ FreshmobTools/FreshmobTools.h
@@ -0,0 +1,19 @@
+//
+//  FreshmobTools.h
+//  FreshmobTools
+//
+//  Created by Rogerio de Paula Assis on 17/11/16.
+//  Copyright © 2016 Rogerio de Paula Assis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FreshmobTools.
+FOUNDATION_EXPORT double FreshmobToolsVersionNumber;
+
+//! Project version string for FreshmobTools.
+FOUNDATION_EXPORT const unsigned char FreshmobToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FreshmobTools/PublicHeader.h>
+
+
